a2a026e1b7ede781be120c01ca49571c31d4e026
Renzo Herrera Giuria <Renzo Herrera Giuria@DESKTOP-FPEFDKS>
2023-04-09 10:23:16 -0500

    Agregar archivos de proyecto.

---
+++ ifexample/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int totalJugador = 22;
+int totalDealer = 15;
+string message = "";
+
+//Blackjack, si tienes mas puntos que el lider ganas, pero si tienes menos o te pasas de 21 entonces pierdes
+
+Console.WriteLine($"El jugador tiene {totalJugador}");
+Console.WriteLine($"El dealer tiene {totalDealer}");
+if ((totalJugador > totalDealer) && totalJugador <= 21)
+{
+    message = "Venciste al Dealer";
+}
+
+else if ((totalJugador < totalDealer) || totalJugador >= 22)
+{
+    message = "La casa gana, mejor suerte para la proxima";
+}
+
+else if (totalJugador == totalDealer)
+{
+    message = "Empataron, por poco y no la cuentas";
+}
+Console.WriteLine(message);
